8f64d4b6e40fea51d1eea16b8bc2dd67b803208f
Ko3uHaK <den2001888@yandex.by>
2021-11-20 09:16:37 +0300

    firt manipulation with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <div>
+	<p>Как дела</p>
+    <p>how old are you?</p>
+    <p>what is your name ?</p>
+	<input/><br/>
+	<input/><br/>
+	</div>;
 }
 
 export default App;
