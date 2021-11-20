708e14b9a3242dd834e92803bb5e3ef71b75ba0a
Ko3uHaK <den2001888@yandex.by>
2021-11-20 09:27:48 +0300

    some practic

--- src/App.js
+++ src/App.js
@@ -7,6 +7,39 @@ function App() {
     <p>what is your name ?</p>
 	<input/><br/>
 	<input/><br/>
+	<ul>
+		<li>1</li>
+		<li>2</li>
+		<li>3</li>
+		<li>4</li>
+		<li>5</li>
+		<li>6</li>
+		<li>7</li>
+		<li>8</li>
+		<li>9</li>
+	</ul>
+	<table>
+		<tbody>
+			<tr>
+				<td>1</td>
+				<td>2</td>
+				<td>3</td>
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>2</td>
+				<td>3</td>
+			</tr>
+			<tr>
+				<td>1</td>
+				<td>2</td>
+				<td>3</td>
+			</tr>
+		</tbody>
+	</table>
+	<p className="pop">pop</p>
+	<p className="pip">pip</p>
+	<p className="pap">pap</p>
 	</div>;
 }
 
