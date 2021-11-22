81b3ef2b1d151b2b2d177dabec10b78bc848d630
Ko3uHaK <den2001888@yandex.by>
2021-11-22 11:34:14 +0300

    react teg 1

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
 import React from 'react';
 
 function App() {
-	return <div>
-	<p>Как дела</p>
-    <p>how old are you?</p>
-    <p>what is your name ?</p>
-	<input/><br/>
-	<input/><br/>
-	<ul>
-		<li>1</li>
-		<li>2</li>
-		<li>3</li>
-		<li>4</li>
-		<li>5</li>
-		<li>6</li>
-		<li>7</li>
-		<li>8</li>
-		<li>9</li>
+	return (
+		<ul>
+		<li>text1</li>
+		<li>text2</li>
+		<li>text3</li>
 	</ul>
-	<table>
-		<tbody>
-			<tr>
-				<td>1</td>
-				<td>2</td>
-				<td>3</td>
-			</tr>
-			<tr>
-				<td>1</td>
-				<td>2</td>
-				<td>3</td>
-			</tr>
-			<tr>
-				<td>1</td>
-				<td>2</td>
-				<td>3</td>
-			</tr>
-		</tbody>
-	</table>
-	<p className="pop">pop</p>
-	<p className="pip">pip</p>
-	<p className="pap">pap</p>
-	</div>;
+	);
 }
 
 export default App;
