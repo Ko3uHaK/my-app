8988b2aff41ed4d415421c8be38050cd30c70c4e
Ko3uHaK <den2001888@yandex.by>
2021-11-22 11:44:46 +0300

    react tag 3

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import React from 'react';
 
 function App() {
 	return (
-		<>
-	<ul>
-		<li>text1</li>
-		<li>text2</li>
-		<li>text3</li>
-	</ul>
-	<ul>
-		<li>text1</li>
-		<li>text2</li>
-		<li>text3</li>
-	</ul>;
-		</>
-	);
+		<input />
+	)
+	// (
+	// 	<>
+	// <ul>
+	// 	<li>text1</li>
+	// 	<li>text2</li>
+	// 	<li>text3</li>
+	// </ul>
+	// <ul>
+	// 	<li>text1</li>
+	// 	<li>text2</li>
+	// 	<li>text3</li>
+	// </ul>;
+	// 	</>
+	// );
 	// (
 	// 	<ul>
 	// 	<li>text1</li>
