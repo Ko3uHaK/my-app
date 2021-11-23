0c7fe959b9769e8c2e221390f9d9121b9bf220e8
Ko3uHaK <den2001888@yandex.by>
2021-11-23 09:56:48 +0300

    react const 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
 function App() {
-	const str1 = 'text1';
-	const str2 = 'text1';
-	return (
-		<div>
-		<p>{str1}</p>
-		<p>{str2}</p>
-	</div>
-	);
+	const name = 'user';
+	const age  = '30';
+	
+	return <div>
+		name: {name}<br/>
+		age:  {age}
+	</div>;
+	// const str1 = 'text1';
+	// const str2 = 'text1';
+	// return (
+	// 	<div>
+	// 	<p>{str1}</p>
+	// 	<p>{str2}</p>
+	// </div>
+	// );
 	// (
 	// 	<>
 	// 		<input/>
