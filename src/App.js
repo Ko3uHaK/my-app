87f57c420da7da497d9db5efc1e00b4fc40fb819
Ko3uHaK <den2001888@yandex.by>
2021-11-24 09:43:18 +0300

    react if

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 function App() {
-	const str1 = 'label';
-	const str2 = 'block';
-	const str3 = 'elem';
+	let text;
+	const isAdult = true;
+	if(isAdult){
+		text = <p>yes</p>;
+	}else{
+		text =<p>no</p>;
+	}
+	return text;
+	// const str1 = 'label';
+	// const str2 = 'block';
+	// const str3 = 'elem';
 	
-	return <div>
-		<label id={str1} htmlFor={str2} className={str3}>text</label> <br/>
-		<input id={str2}/>
-	</div>;
+	// return <div>
+	// 	<label id={str1} htmlFor={str2} className={str3}>text</label> <br/>
+	// 	<input id={str2}/>
+	// </div>;
 	// const class1 = 'block1';
 	// const class2 = 'block2';
 	
