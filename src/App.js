37c74516698f402f17078462e7b6f4263b51c374
Ko3uHaK <den2001888@yandex.by>
2021-11-26 13:03:12 +0300

    react event 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
 function App() {
-		function showMess() {
-			alert('hello');
-		}
+	function show1() {
+		alert(1);
+	}
+	
+	function show2() {
+		alert(2);
+	}
+	
+	return <div>
+		<button onClick={show1}>act1</button>
+		<button onClick={show2}>act2</button>
+	</div>;
+		// function showMess() {
+		// 	alert('hello');
+		// }
 		
-		return <div>
-			<button onClick={showMess}>show</button>
-		</div>;
+		// return <div>
+		// 	<button onClick={showMess}>show</button>
+		// </div>;
 	// function getDigitsSum(num){
 	// 	let string = num.toString();
 	// 	let arr = string.split("");
