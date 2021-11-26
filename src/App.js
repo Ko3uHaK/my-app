a10ca224bf47fcd14cedc88263988cfaa5f8e37b
Ko3uHaK <den2001888@yandex.by>
2021-11-26 13:14:59 +0300

    react event 3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 
 function App() {
-	function show1() {
-		alert(1);
+	function show(num){
+		alert(num);
 	}
-	
-	function show2() {
-		alert(2);
-	}
-	
 	return <div>
-		<button onClick={show1}>act1</button>
-		<button onClick={show2}>act2</button>
-	</div>;
+	<button onClick = {()=>show(1)}>act1</button>
+	<button onClick = {()=>show(2)}>act2</button>
+	<button onClick = {()=>show(3)}>act3</button>
+</div>;
+	// function show1() {
+	// 	alert(1);
+	// }
+	
+	// function show2() {
+	// 	alert(2);
+	// }
+	
+	// return <div>
+	// 	<button onClick={show1}>act1</button>
+	// 	<button onClick={show2}>act2</button>
+	// </div>;
 		// function showMess() {
 		// 	alert('hello');
 		// }
