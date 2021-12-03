bb6b582482f771cbc825e97aa92aa3e2d730d798
Ko3uHaK <den2001888@yandex.by>
2021-12-03 16:34:07 +0300

    react arr id

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 function App() {
 	const users = [
-		{name: 'user1', surn: 'surn1', age: 30},
-		{name: 'user2', surn: 'surn2', age: 31},
-		{name: 'user3', surn: 'surn3', age: 32},
+		{id:1,name: 'user1', surn: 'surn1', age: 30},
+		{id:2,name: 'user2', surn: 'surn2', age: 31},
+		{id:3,name: 'user3', surn: 'surn3', age: 32},
 	];
-	const res = users.map(function(item, index) {
-		return <li key={index}>
+	const res = users.map(function(item) {
+		return <li key={item.id}>
 		<span>{item.name}</span>:
 		<span>{item.surn}</span>
 		</li>;
