62b59feb6c5dc86c992d68b08e832bc851282d66
Ko3uHaK <den2001888@yandex.by>
2021-12-26 15:35:26 +0300

    react input 1.1

--- src/App.js
+++ src/App.js
@@ -3,22 +3,34 @@ import { nanoid } from 'nanoid';
 import React, {useState} from 'react';
 
 function App() {
-	const [value1, setValue1] = useState(0);
-	const [value2, setValue2] = useState(0);
-	const [value3, setValue3] = useState(0);
-	const [value4, setValue4] = useState(0);
-	const [value5, setValue5] = useState(0);
+
+		const [value1, setValue1] = useState(0);
+		const [value2, setValue2] = useState(0);
+		const [result, setResult] = useState(0);
+		
+		return <div>
+			<input value={value1} onChange={event => setValue1(event.target.value)} />
+			<input value={value2} onChange={event => setValue2(event.target.value)} />
+			
+			<button onClick={() => setResult(Number(value1) + Number(value2))}>btn</button>
+			<p>result: {result}</p>
+		</div>;
+	// const [value1, setValue1] = useState(0);
+	// const [value2, setValue2] = useState(0);
+	// const [value3, setValue3] = useState(0);
+	// const [value4, setValue4] = useState(0);
+	// const [value5, setValue5] = useState(0);
 	
 
 	
-	return <div>
-	<input value={value1} onChange={event => setValue1(+event.target.value)}/>
-	<input value={value2} onChange={event => setValue2(+event.target.value)}/>
-	<input value={value3} onChange={event => setValue3(+event.target.value)}/>
-	<input value={value4} onChange={event => setValue4(+event.target.value)}/>
-	<input value={value5} onChange={event => setValue5(+event.target.value)}/>
-	<p>average: {(value1 + value2+ value3+value4+value5)/5}</p>
-	</div>
+	// return <div>
+	// <input value={value1} onChange={event => setValue1(+event.target.value)}/>
+	// <input value={value2} onChange={event => setValue2(+event.target.value)}/>
+	// <input value={value3} onChange={event => setValue3(+event.target.value)}/>
+	// <input value={value4} onChange={event => setValue4(+event.target.value)}/>
+	// <input value={value5} onChange={event => setValue5(+event.target.value)}/>
+	// <p>average: {(value1 + value2+ value3+value4+value5)/5}</p>
+	// </div>
 	// const [name, setName] = useState('ALex');
 	// const [age, setAge] = useState(23);
 	// let [inBlock, setInBlock] = useState(false);
