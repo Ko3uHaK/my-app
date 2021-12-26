357cec1e727375179f143a060367b4667e0cf40b
Ko3uHaK <den2001888@yandex.by>
2021-12-26 13:21:09 +0300

    react input 8

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import { nanoid } from 'nanoid';
 import React, {useState} from 'react';
 
 function App() {
-		const [value, setValue] = useState('age');
-		
-		function showAge(){
-			return value - 257;
-		}
-		return <div>
-			<input value={value} onChange={event => setValue(event.target.value)} /><br/>
-			<p>text{showAge(value)}</p>
-		</div>;
+	const [value1, setValue1] = useState(0);
+	const [value2, setValue2] = useState(0);
+	
+	function handleChange1(event) {
+		setValue1(+event.target.value);
+	}
+	
+	function handleChange2(event) {
+		setValue2(+event.target.value);
+	}
+	
+	return <div>
+		<input value={value1} onChange={handleChange1} />
+		<input value={value2} onChange={handleChange2} />
+		<p>result: {value1 + value2}</p>
+	</div>
 	// const [name, setName] = useState('ALex');
 	// const [age, setAge] = useState(23);
 	// let [inBlock, setInBlock] = useState(false);
