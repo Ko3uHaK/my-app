652d2d594ac103cc3d06620531becd24dc9c3c2b
Ko3uHaK <den2001888@yandex.by>
2021-12-26 15:56:29 +0300

    react input 1.3

--- src/App.js
+++ src/App.js
@@ -4,16 +4,15 @@ import React, {useState} from 'react';
 
 function App() {
 
-		const [value1, setValue1] = useState(0);
-		const [value2, setValue2] = useState(0);
+		const [value1, setValue1] = useState(Date);
+		const [value2, setValue2] = useState(Date);
 		const [result, setResult] = useState(0);
 		
 		return <div>
 			<input value={value1} onChange={event => setValue1(event.target.value)} />
 			<input value={value2} onChange={event => setValue2(event.target.value)} />
 			
-			<button onClick={() => setResult(Number(value1) + Number(value2))}>btn</button>
-			<button onClick={() => setResult(Number(value1) * Number(value2))}>btn</button>
+			<button onClick={Math.ceil(value1.getTime() - value2.getTime()) / (1000 * 3600 * 24)}>btn</button>
 			<p>result: {result}</p>
 		</div>;
 	// const [value1, setValue1] = useState(0);
