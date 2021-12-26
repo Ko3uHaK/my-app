5dc6274dbfb3f9f0cd121766dbbcd218c97feecf
Ko3uHaK <den2001888@yandex.by>
2021-12-26 13:28:18 +0300

    react input 9

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import React, {useState} from 'react';
 function App() {
 	const [value1, setValue1] = useState(0);
 	const [value2, setValue2] = useState(0);
+	const [value3, setValue3] = useState(0);
+	const [value4, setValue4] = useState(0);
+	const [value5, setValue5] = useState(0);
 	
-	function handleChange1(event) {
-		setValue1(+event.target.value);
-	}
-	
-	function handleChange2(event) {
-		setValue2(+event.target.value);
-	}
+
 	
 	return <div>
-		<input value={value1} onChange={handleChange1} />
-		<input value={value2} onChange={handleChange2} />
-		<p>result: {value1 + value2}</p>
+	<input value={value1} onChange={event => setValue1(+event.target.value)}/>
+	<input value={value2} onChange={event => setValue2(+event.target.value)}/>
+	<input value={value3} onChange={event => setValue3(+event.target.value)}/>
+	<input value={value4} onChange={event => setValue4(+event.target.value)}/>
+	<input value={value5} onChange={event => setValue5(+event.target.value)}/>
+	<p>average: {(value1 + value2+ value3+value4+value5)/5}</p>
 	</div>
 	// const [name, setName] = useState('ALex');
 	// const [age, setAge] = useState(23);
