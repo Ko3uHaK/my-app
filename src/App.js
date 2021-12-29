564c136165a5b7e13f156d5ad5145dec2279edbb
Ko3uHaK <den2001888@yandex.by>
2021-12-29 11:16:36 +0300

    react textarea

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import { nanoid } from 'nanoid';
 import React, {useState} from 'react';
 
 function App() {
-
-		const [value1, setValue1] = useState(Date);
-		const [value2, setValue2] = useState(Date);
-		const [result, setResult] = useState(0);
+	const [value, setValue] = useState(0);
+	
+	return <div>
+		<textarea value={value} onChange={event => setValue(+event.target.value)} />
+		<p>{value+value}</p>
+	</div>;
+		// const [value1, setValue1] = useState(0);
+		// const [result, setResult] = useState(0);
 		
-		return <div>
-			<input value={value1} onChange={event => setValue1(event.target.value)} />
-			<input value={value2} onChange={event => setValue2(event.target.value)} />
-			
-			<button onClick={Math.ceil(value1.getTime() - value2.getTime()) / (1000 * 3600 * 24)}>btn</button>
-			<p>result: {result}</p>
-		</div>;
+		// return <div>
+		// 	<input value={value1} onChange={event => setValue1(event.target.value)} />
+		// 	<button onClick={}>btn</button>
+		// 	<p>result: {result}</p>
+		// </div>;
 	// const [value1, setValue1] = useState(0);
 	// const [value2, setValue2] = useState(0);
 	// const [value3, setValue3] = useState(0);
